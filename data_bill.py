689f76b3055fe3c95bde793587ae5677c92c30a2
Moon <81275809+renat5000@users.noreply.github.com>
2026-05-13 13:01:20 -0700

    The Start inputs/variables

--- data_bill.py
+++ data_bill.py
@@ -8,3 +8,10 @@ REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
 
 
 # Your code goes here:
+
+data_usage_gb = float(input("Enter the data usage in GB: "))
+monthly_plan = int(input("Enter the monthly plan cost: "))
+premium_user_input = input("Is the user a premium user? (yes/no): ").strip().lower()
+is_premium_user = premium_user_input == "yes"
+total_bill = monthly_plan
+
